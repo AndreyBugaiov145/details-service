cd2b5b0123ff45b183008b64f74a2c2cae452583
buhaiov <a.buhaiov@urancompany.com>
2023-02-04 19:32:11 +0200

    add Domparser

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ParsingSettingController;
 use App\Http\Controllers\Users;
 use Illuminate\Support\Facades\Route;
+use PHPHtmlParser\Dom;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,25 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+
+Route::get('/test', function () {
+    $html = file_get_contents('https://habr.com/ru/company/vk/blog/565028/');
+    $dom = new Dom;
+    $dom->loadStr($html);
+
+    $a   = $dom->find('.tm-article-snippet__title.tm-article-snippet__title_h1');
+    dd($a->firstChild()->text);
+//    $rez = pq($doc,'title');
+
+//    dd($rez);
+//    phpQuery::unloadDocuments($doc);
+
+    echo $html;
+
+    return 1;
+});
+
 
 Route::get('/', function () {
     return view('home');

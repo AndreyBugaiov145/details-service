5b97f8a0f510cbd9fdc7b542a712abe6e0ea857f
buhaiov <a.buhaiov@urancompany.com>
2023-04-10 23:51:57 +0300

    revert home page

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+Route::get('/', function () {
+    return view('home');
+})->name('home');
+
 Route::prefix('login')->group(function () {
     Route::get('/', function () {
         return view('auth.sign-in');

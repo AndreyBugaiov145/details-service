302613feec3083f4e2c9203e4da3b88ad118d46b
buhaiov <a.buhaiov@urancompany.com>
2023-04-10 20:41:38 +0300

    fix redirect after login

--- app/Http/Controllers/Users.php
+++ app/Http/Controllers/Users.php
@@ -21,7 +21,7 @@ class Users extends Controller
         if (Auth::attempt($credentials, $request->get('remember'))) {
             $request->session()->regenerate();
 
-            return redirect()->intended('home');
+            return redirect()->intended('admin');
         }
 
         return back()->withErrors([
--- routes/web.php
+++ routes/web.php
@@ -23,11 +23,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
-Route::get('/', function () {
-    return view('home');
-})->name('home');
-
 Route::prefix('login')->group(function () {
     Route::get('/', function () {
         return view('auth.sign-in');

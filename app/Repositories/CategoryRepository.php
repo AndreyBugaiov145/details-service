e54afdbc5b01a3fff92840a207a26776056ecaaf
buhaiov <a.buhaiov@urancompany.com>
2023-04-18 21:09:51 +0300

    fix API get category list

--- app/Repositories/CategoryRepository.php
+++ app/Repositories/CategoryRepository.php
@@ -14,9 +14,9 @@ class CategoryRepository
     public static function getLastChildrenCategories($category_title, $year)
     {
         $category = Category::where('title', $year)
-            ->whereHas('parent',function ($query) use ($category_title){
+            ->whereHas('parent', function ($query) use ($category_title) {
                 $query->where('title', $category_title);
-        })
+            })
             ->withAllChildren()->first();
         $categories = self::getLastChildren([$category->toArray()]);
 
@@ -42,15 +42,12 @@ class CategoryRepository
     {
         $categories = Category::where('parent_id', 0)->select(['categories.title', 'categories.id'])
             ->join('parsing_settings', function (JoinClause $join) {
-                $join->on('parsing_settings.brand', 'like', 'categories.title')->where('parsing_settings.is_show', true);
+                $join->on('parsing_settings.brand', 'like', 'categories.title')
+                    ->where('parsing_settings.is_show', true)
+                    ->whereNull('deleted_at');
             })->get();
 
-        $categories = $categories->unique('id')->map(function ($item) {
-            $item->child_type = self::CHILD_TYPE_YEAR;
-            return $item;
-        });
-
-        return $categories;
+        return $categories->unique('id');
     }
 
     public static function getChildrenCategories($id, $child_type = null)
@@ -66,7 +63,7 @@ class CategoryRepository
 
     public static function getChildMainYearsCategories($id)
     {
-        $parentMainCategory = Category::select(['title','id'])->sortByDesc('title')->find($id);
+        $parentMainCategory = Category::select(['title', 'id'])->find($id);
         if (is_null($parentMainCategory)) {
             return collect();
         }
@@ -76,9 +73,9 @@ class CategoryRepository
                 $join->on('parsing_settings.year', 'categories.title')
                     ->where('parsing_settings.brand', 'like', $parentMainCategory->title)
                     ->where('parsing_settings.is_show', true);
-            })->get();
+            })->orderByDesc('title')->get();
 
-        $categories = $categories->unique('id')->$categories->map(function ($item) {
+        $categories = $categories->unique('id')->map(function ($item) {
             $item->child_type = self::CHILD_TYPE_MODEL;
             return $item;
         });
@@ -97,7 +94,7 @@ class CategoryRepository
         if (is_null($parentMainYarCategory)) {
             return collect();
         }
-        $categories = Category::select(['title','id'])->where('parent_id', $id)->get();
+        $categories = Category::select(['title', 'id'])->where('parent_id', $id)->get();
 
         $parsingSetting = ParsingSetting::where([
             ['brand', 'like', $parentMainCategory->title],
@@ -107,16 +104,18 @@ class CategoryRepository
             return collect();
         }
 
-        $categories = $categories->filter(function ($category, $key) use ($parsingSetting) {
-            return str_contains($parsingSetting->car_models, $category->title);
-        });
+        if (strlen( $parsingSetting->car_models)) {
+            $categories = $categories->filter(function ($category, $key) use ($parsingSetting) {
+                return str_contains($parsingSetting->car_models, $category->title);
+            });
+        }
 
         return $categories->unique('id');
     }
 
     public static function getChildCategories($id)
     {
-        $categories = Category::select(['title','id'])->where('parent_id', $id)->orderByDesc('title')->get();
+        $categories = Category::select(['title', 'id'])->where('parent_id', $id)->orderByDesc('title')->get();
 
         return $categories;
     }

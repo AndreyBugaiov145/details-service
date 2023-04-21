10130938c1e7166c2d941ac4f2851d4acd9f75ff
buhaiov <a.buhaiov@urancompany.com>
2023-04-21 18:14:42 +0300

    add parent_id index

--- database/migrations/2023_02_02_165307_create_categories_table.php
+++ database/migrations/2023_02_02_165307_create_categories_table.php
@@ -16,9 +16,10 @@ class CreateCategoriesTable extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->index('title');
+//            $table->index('title');
             $table->text('jsn')->nullable();
             $table->unsignedBigInteger('parent_id')->unsigned()->default(0);
+            $table->index('parent_id');
 //            $table->foreign('parent_id')->references('id')->on('categories')->onDelete('cascade');
             $table->unique(['title','parent_id']);
             $table->timestamps();

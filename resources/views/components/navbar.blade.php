fb4f84c943f45858c48bc55fa419921990215d56
buhaiov <a.buhaiov@urancompany.com>
2023-04-22 11:32:47 +0300

    show Exchange Rates

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom-3 p-3 mb-0">
+<nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom-3 p-3 mb-0 d-flex justify-content-between">
     <ul class="navbar-nav mr-auto d-flex flex-row bd-highlight">
         <li class="nav-item mr-3">
             <a class="nav-link {{ request()->is('admin') ? 'active' : '' }}" href="/">Home</a>
@@ -13,4 +13,14 @@
             <a class="nav-link {{ request()->is('admin/logout') ? 'active' : '' }}" href="{{ route('logout') }}">Logout</a>
         </li>
     </ul>
+    <div class="d-flex justify-content-end">
+       <span style="font-weight: 600"> Курс валют  &nbsp; &nbsp; </span>
+        @foreach (  $currencies as $i => $current)
+            <span class="bi-text-right">{{$current->rate}}{{$current->symbol}}</span>
+            @if($i < (count($currencies) - 1))
+                <span>=</span>
+            @endif
+        @endforeach
+    </div>
+
 </nav>

5d76c927d364a8299066e99f0de704415c90dc51
buhaiov <a.buhaiov@urancompany.com>
2023-04-25 11:21:13 +0300

    change  job  tries to 2

--- app/Jobs/GrabbingCategoriesAndDetails.php
+++ app/Jobs/GrabbingCategoriesAndDetails.php
@@ -19,7 +19,7 @@ class GrabbingCategoriesAndDetails implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-    public $tries = 1;
+    public $tries = 2;
 
     public $timeout = 17000;
 
